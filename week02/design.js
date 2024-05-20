aee56da3a0104994a78f1437f5deba269b7ba348
DRausseo <dannymrleon@yahoo.com>
2024-05-20 13:55:15 -0300

    crear carpeta chamber, indexhtml y ciodigos css base y larger

--- week02/design.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamButton = document.getElementById('menu');
-    const navigation = document.querySelector('.navigation');
-
-    hamButton.addEventListener('click', function() {
-        navigation.classList.toggle('open');
-        hamButton.classList.toggle('open');
-    });
-});
